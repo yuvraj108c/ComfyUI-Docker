e7460ac0d5fb655d3fbbf02e993ecc7143e7b4a1
yuvraj108c <yuvraj108c@gmail.com>
2024-07-14 09:55:46 +0000

    update comfyui

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -4,4 +4,4 @@ apt update && apt install -y openssh-server
 mkdir /var/run/sshd
 
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
